63e84173c80032b5220f9dd08b4bf91a1379bd8a
HomerusJa <jakob@schluse.com>
2024-10-14 10:47:21 +0200

    Added subtitle support and updated poetry.lock

--- core/youtube.py
+++ core/youtube.py
@@ -7,42 +7,59 @@ Uploader -> YouTubeDriveUploader
 
 import yt_dlp
 
+from core import base
+
+DEFAULT_YDL_OPTS = {
+    'format': 'best',
+    'outtmpl': './downloads/%(title)s.%(ext)s',
+    'quiet': True,
+    'postprocessors': [{'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4'}]
+}
+
 
 class YouTubeDownloader:
-    def __init__(self):
-        self.ydl_opts = {
-            'format': 'best',
-            'outtmpl': './downloads/%(title)s.%(ext)s',
-            'quiet': True,
-            'postprocessors': [{'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4'}]
-        }
+    @staticmethod
+    def download_video(self, url: str, subtitles: bool = False, subtitle_lang: str = 'en'):
+        ydl_opts = DEFAULT_YDL_OPTS.copy()
+        if subtitles:
+            self.add_subtitle_options(ydl_opts, subtitle_lang)
 
-    def download_video(self, url):
-        with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
             return ydl.prepare_filename(info_dict)
 
-    def download_playlist(self, url):
-        self.ydl_opts['noplaylist'] = False
-        with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+    @staticmethod
+    def download_playlist(self, url: str, subtitles: bool = False, subtitle_lang: str = 'en'):
+        ydl_opts = DEFAULT_YDL_OPTS.copy()
+        ydl_opts['noplaylist'] = False
+        if subtitles:
+            self.add_subtitle_options(ydl_opts, subtitle_lang)
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
             return [ydl.prepare_filename(entry) for entry in info_dict['entries']]
 
+    @staticmethod
+    def add_subtitle_options(ydl_opts: dict, subtitle_lang: str):
+        ydl_opts['writesubtitles'] = True
+        ydl_opts['subtitleslangs'] = [subtitle_lang]
+
+
 class YouTubeDriveUploader:
-    def __init__(self, drive_service):
+    def __init__(self, drive_service: base.Service):
         self.drive_service = drive_service
 
-    def upload_video(self, video_path):
+    def upload_video(self, video_path: str):
         folder_id = self._get_or_create_folder('Youtube Videos')
         self.drive_service.upload_file(video_path, folder_id)
 
-    def upload_playlist(self, video_paths, playlist_title):
+    def upload_playlist(self, video_paths: list[str], playlist_title: str):
         parent_folder_id = self._get_or_create_folder('Youtube Videos')
         playlist_folder_id = self._get_or_create_folder(playlist_title, parent_folder_id)
         for video_path in video_paths:
             self.drive_service.upload_file(video_path, playlist_folder_id)
 
-    def _get_or_create_folder(self, folder_name, parent_id=None):
+    def _get_or_create_folder(self, folder_name: str, parent_id=None):
         query = f"name = '{folder_name}' and mimeType = 'application/vnd.google-apps.folder'"
         if parent_id:
             query += f" and '{parent_id}' in parents"
